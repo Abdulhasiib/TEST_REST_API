11fe5e883fb4063743c404f8a4a1ed25aad5f99e
Mohammed Haseeb <Mohammed.Haseeb@aveva.com>
2019-10-09 00:38:36 +0530

    Initial Test - GET

---
+++ TEST_REST_API/LocationAPI.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RestSharp;
+using RestSharp.Authenticators;
+
+namespace TEST_REST_API
+{
+    [TestClass]
+    public class LocationAPI
+    {
+        [TestMethod]
+        public void GetLocationInfo()
+        {
+            //Client Initialisation
+            const string baseURL = "http://api.zippopotam.us/IN/";
+            RestClient restClient = new RestClient();
+            restClient.BaseUrl = new Uri(baseURL);
+
+            //Request Data from Server
+            RestRequest resRequest = new RestRequest("500032", Method.GET);
+
+            //Execute Request and check server response
+            IRestResponse restResponse = restClient.Execute(resRequest);
+
+            //Extracting data from responce
+            string location = restResponse.Content;
+
+            //Evaluating Response
+            if (location.Contains("Gachibowli"))
+            {
+                Assert.IsTrue(true);
+            }
+            else
+            {
+                Assert.Fail("Location Information not correct");
+            }
+            
+        }
+    }
+}
---
+++ TEST_REST_API/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TEST_REST_API")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TEST_REST_API")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("37c39170-8883-4754-8f94-be243ab0342f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
